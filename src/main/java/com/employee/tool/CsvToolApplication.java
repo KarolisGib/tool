05619307de883d4ce9800bd7beddf205909971cd
KarolisGib <karolisgibavicius@yahoo.com>
2023-02-19 23:59:42 +0200

    Initial commit

---
+++ src/main/java/com/employee/tool/CsvToolApplication.java
@@ -0,0 +1,13 @@
+package com.employee.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employee/tool/CsvToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
