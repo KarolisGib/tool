8f2418b0284faedd729fc2c688ee7b292214a9de
KarolisGib <karolisgibavicius@yahoo.com>
2023-02-21 12:14:10 +0200

    Add getAllEmployees method to EmployeeService file

--- src/main/java/com/employee/tool/service/EmployeeService.java
+++ src/main/java/com/employee/tool/service/EmployeeService.java
@@ -1,4 +1,19 @@
 package com.employee.tool.service;
 
+import com.employee.tool.repository.EmployeeRepository;
+import com.employee.tool.repository.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return (List<Employee>) employeeRepository.findAll();
+    }
 }
