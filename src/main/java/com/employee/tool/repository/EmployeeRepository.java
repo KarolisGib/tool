590fcf6650fa8dbbe712f56fadafe5e26b7f110e
KarolisGib <karolisgibavicius@yahoo.com>
2023-02-23 05:19:40 +0200

    Add findAll method in EmployeeRepository

--- src/main/java/com/employee/tool/repository/EmployeeRepository.java
+++ src/main/java/com/employee/tool/repository/EmployeeRepository.java
@@ -1,4 +1,11 @@
 package com.employee.tool.repository;
 
-public interface EmployeeRepository {
+import com.employee.tool.repository.model.Employee;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+
+    List<Employee> findAll();
 }
