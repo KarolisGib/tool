369ec6bd3d8991fca8192f031eacb361ca4ad573
KarolisGib <karolisgibavicius@yahoo.com>
2023-02-23 05:19:07 +0200

    Add data to POJO class

--- src/main/java/com/employee/tool/repository/model/Employee.java
+++ src/main/java/com/employee/tool/repository/model/Employee.java
@@ -1,4 +1,30 @@
 package com.employee.tool.repository.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class Employee {
+
+    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "phone")
+    private String phone;
 }
