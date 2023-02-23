a637288ea3a8d8ab7933a0cd6b542d4922c5cf06
KarolisGib <karolisgibavicius@yahoo.com>
2023-02-23 05:21:21 +0200

    Add getAllEmployees method with mapping

--- src/main/java/com/employee/tool/controller/EmployeeController.java
+++ src/main/java/com/employee/tool/controller/EmployeeController.java
@@ -1,4 +1,25 @@
 package com.employee.tool.controller;
 
+import com.employee.tool.repository.model.Employee;
+import com.employee.tool.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/mapping") //http://localhost:8080/mapping
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping(path = "/get/employee/all") //http://localhost:8080/mapping/get/employee/all
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
 }
--- src/main/java/com/employee/tool/service/EmployeeService.java
+++ src/main/java/com/employee/tool/service/EmployeeService.java
@@ -2,7 +2,6 @@ package com.employee.tool.service;
 
 import com.employee.tool.repository.EmployeeRepository;
 import com.employee.tool.repository.model.Employee;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,10 +9,13 @@ import java.util.List;
 @Service
 public class EmployeeService {
 
-    @Autowired
-    private EmployeeRepository employeeRepository;
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 
     public List<Employee> getAllEmployees() {
-        return (List<Employee>) employeeRepository.findAll();
+        return employeeRepository.findAll();
     }
 }
